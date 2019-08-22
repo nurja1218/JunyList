0fe8439c78ef0cc0e6a6e7588d38dcd65b140a47
juny <nurja1218@gmail.com>
2019-08-22 15:48:40 +0900

    make basic file

---
+++ index.js
@@ -0,0 +1,13 @@
+const junyInfo = {
+    name: "junYoung",
+    age: 30,
+    gender: "male",
+    livenIn: "Guri",
+    favMovies: []
+};
+
+console.log(junyInfo.gender);
+
+junyInfo.gender = "female";
+
+console.log(junyInfo.gender);
