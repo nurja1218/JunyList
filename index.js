1f06041c0806c87793c6b7886980c835e25d9e6f
juny <nurja1218@gmail.com>
2019-08-25 21:23:40 +0900

    function change / classList -> toggle

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const title = document.querySelector("#title");
 const CLICK_CLASS = "clicked";
 
 function handleClick() {
-    const hasClass = title.classList.contains(CLICK_CLASS);
-    if(hasClass === false){
-        title.classList.add(CLICK_CLASS);
-    } else {
-        title.classList.remove(CLICK_CLASS);
-    }
+    title.classList.toggle(CLICK_CLASS);
 }
 
 function init() {
