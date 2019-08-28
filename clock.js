739031b2a58d5a9f71844f3d230a51c9e9fa8300
juny <nurja1218@gmail.com>
2019-08-28 17:04:49 +0900

    form of time modified

--- clock.js
+++ clock.js
@@ -5,8 +5,13 @@ function getTime() {
     const date = new Date();
     const seconds = date.getSeconds();
     const minutes = date.getMinutes();
-    const housrs = date.getHours();
-    clockTitle.innerText = `${housrs}:${minutes}:${seconds < 10?`0${seconds}`:seconds}`;
+    const hours = date.getHours();
+
+    clockTitle.innerText =  `${hours < 10 ? `am0${hours}` :
+        hours < 13 ? `am${hours}` : hours < 22 ? `pm0${hours-12}`
+        : `pm${hours-12}`}:${
+        minutes < 10?`0${minutes}`: minutes}:${
+        seconds < 10?`0${seconds}`: seconds}`;
 }
 
 function init() {
